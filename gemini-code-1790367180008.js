1f1c3391a8d92f435fc17aca57a8dda6504a8b66
mendylemel-star <mendylemel@gmail.com>
2026-09-25 16:15:24 -0400

    Add files via upload

---
+++ gemini-code-1790367180008.js
@@ -0,0 +1,17 @@
+const CACHE_NAME = 'my-site-cache';
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cachedResponse) => {
+      if (cachedResponse) {
+        return cachedResponse;
+      }
+      return fetch(event.request).then((networkResponse) => {
+        return caches.open(CACHE_NAME).then((cache) => {
+          cache.put(event.request, networkResponse.clone());
+          return networkResponse;
+        });
+      });
+    })
+  );
+});
